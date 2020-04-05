afe3ff233ecfe0966c0a6ed08a7d7f674d5324ce
Vitor Tavares <atavares.vitor@gmail.com>
2020-04-05 02:10:57 -0300

    tried to add route to show the info sent, but no success

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  post '/invoice', to: 'pages#create'
+  get '/invoice', to: 'pages#invoice'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
