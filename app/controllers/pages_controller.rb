8ead70570efff6f0fc8c45384013e569a294fa51
Andre Ferrer <andrerferrer@hotmail.com>
2020-04-06 15:56:37 -0300

    move controller logic from pages to invoices controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,3 @@
 class PagesController < ApplicationController
-   skip_before_filter :verify_authenticity_token  
-  # Method to receive the post
-  def create
-    raise
-  end
 
-  # Method to show the information sent
-  def invoice
-  end
-
-  def home
-  end
 end
