49abbfb229d6813120c50caec361aae975184218
Vitor Tavares <atavares.vitor@gmail.com>
2020-04-06 01:58:14 -0300

    tried editting and checking if the missing action error was solved. It is not

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
    skip_before_filter :verify_authenticity_token  
   # Method to receive the post
   def create
+    raise
   end
 
   # Method to show the information sent
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  # root to: 'pages#home'
+  # resources :pages, only: [:new, :create, :show]
 
+  get '/', to: 'pages#home', as: :root
   post '/invoice', to: 'pages#create'
   get '/invoice', to: 'pages#invoice'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
