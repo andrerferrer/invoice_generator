1e622d9479ca601981c7a6084f3de2243c5a76ce
Vitor Tavares <atavares.vitor@gmail.com>
2020-04-06 17:47:23 -0300

    added action index to show the content sent in the home page

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -3,6 +3,13 @@ class InvoicesController < ApplicationController
   end
 
   def create
-    raise
+    @params = params.permit(:company_name, :company_cnpj, :address, :address2, :company_city,
+                              :company_state, :postal_code, :nire, :invoice_number, :invoice_date,
+                              :buyer_name, :buyer_registry, :buyer_address, :service_description, :service_value)
+    redirect_to controller: "invoices", action: "index", params: @params
+  end
+
+  def index
+    @invoice = params
   end
 end
