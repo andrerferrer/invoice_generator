090fff08e4c883daa90ae637a9b2840d1c15453d
Andre Ferrer <andrerferrer@hotmail.com>
2020-04-07 12:47:20 -0300

    Rewrite invoices#index as invoices#show

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -7,10 +7,12 @@ class InvoicesController < ApplicationController
     session[:invoice] = params.permit(:company_name, :company_cnpj, :address, :address2, :company_city,
                             :company_state, :postal_code, :nire, :invoice_number, :invoice_date,
                             :buyer_name, :buyer_registry, :buyer_address, :service_description, :service_value)
-    redirect_to controller: 'invoices', action: 'index'
+    
+    # the magic number is just a placeholder for later
+    redirect_to invoice_path(1)
   end
 
-  def index
+  def show
     @invoice = session[:invoice]
   end
 end
