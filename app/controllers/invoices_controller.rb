1b51a33c3bd03592129ceedaf61a318ae60f5456
Andre Ferrer <andrerferrer@hotmail.com>
2020-04-07 12:43:58 -0300

    Refactor with cached data

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -4,13 +4,13 @@ class InvoicesController < ApplicationController
   def new; end
 
   def create
-    @params = params.permit(:company_name, :company_cnpj, :address, :address2, :company_city,
+    session[:invoice] = params.permit(:company_name, :company_cnpj, :address, :address2, :company_city,
                             :company_state, :postal_code, :nire, :invoice_number, :invoice_date,
                             :buyer_name, :buyer_registry, :buyer_address, :service_description, :service_value)
-    redirect_to controller: 'invoices', action: 'index', params: @params
+    redirect_to controller: 'invoices', action: 'index'
   end
 
   def index
-    @invoice = params
+    @invoice = session[:invoice]
   end
 end
