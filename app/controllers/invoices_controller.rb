3eacd1b0fc36877b76caee49ea843e768daa0982
Vitor Tavares <atavares.vitor@gmail.com>
2020-04-13 20:05:49 -0300

    Added strong params and edited it to be able to store several values

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -4,10 +4,8 @@ class InvoicesController < ApplicationController
   def new; end
 
   def create
-    session[:invoice] = params.permit(:company_name, :company_cnpj, :address, :address2, :company_city,
-                            :company_state, :postal_code, :nire, :invoice_number, :invoice_date,
-                            :buyer_name, :buyer_address, :buyer_address2, :buyer_city, :buyer_state,
-                            :buyer_postal_code, :buyer_registry, :buyer_address, :service_description, :service_value)
+    # raise
+    session[:invoice] = invoice_params
     
     # the magic number is just a placeholder for later
     redirect_to invoice_path(1)
@@ -15,5 +13,14 @@ class InvoicesController < ApplicationController
 
   def show
     @invoice = session[:invoice]
+  end
+
+  private
+
+  def invoice_params
+    params.permit(:company_name, :company_cnpj, :address, :address2, :company_city,
+                            :company_state, :postal_code, :nire, :invoice_number, :invoice_date,
+                            :buyer_name, :buyer_address, :buyer_address2, :buyer_city, :buyer_state,
+                            :buyer_postal_code, :buyer_registry, :buyer_address, service_description: [], service_value: [])
   end
 end
